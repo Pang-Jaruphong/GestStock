419e7e8ee72812b52a7ce7efd03c278b891644de
Pang-Jaruphong <pv82wpf@eduvaud.ch>
2026-03-03 16:17:16 +0100

    getAllArticle join supplier's name and refSupplier

--- backend/db/dbArticles.js
+++ backend/db/dbArticles.js
@@ -5,7 +5,20 @@ const dbArticles = {
         let con;
         try {
             con = await db.connectToDatabase();
-            const sql = `SELECT * FROM articles`
+            // Join suppliers in article
+            const sql =
+                `SELECT a.refArticle,
+                    a.name AS "Nom de l'article", 
+                    a.description,
+                    a.buyPrice AS "Prix d'achat",
+                    a.salePrice AS "Prix de vente",
+                    a.actualStock AS "Stock actuel",
+                    a.minStock AS "Stock minimun",
+                    s.refSupplier,
+                    s.name AS "Nom de fournisseur"
+            FROM articles a
+            JOIN suppliers s ON a.supplier_id = s.id`
+
             const [rows] = await con.query(sql);
             return rows;
         } catch (error) {
